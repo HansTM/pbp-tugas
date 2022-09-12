c6f92a9bb2bbf571911359f0ee7889f934166e22
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-09-12 23:32:15 +0700

    MVP selesai

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+data_katalog = CatalogItem.objects.all()
+context = {
+	'list': data_katalog,
+	'name': 'Hans TM',
+	'id': '2106750295'
+}
+
+def show_catalog(request):
+	return render(request, 'katalog.html', context)
